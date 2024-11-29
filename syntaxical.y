4db81c261d432ceb736f83fff06c04fcdfdc3ed9
imadeddineF <imadeddinefillalipro@gmail.com>
2024-11-29 02:00:56 +0100

    update the right test and add some minor changesin the syntaxical part

--- syntaxical.y
+++ syntaxical.y
@@ -2,7 +2,6 @@
   #include <string.h>
   #include <stdio.h>
   #include <stdlib.h>
-  // #include "quads.h"
   void erreurdd(char* idf);
   void erreur_dim(char* idf);
   int getDimension(char entite[], char scope[]);
--- ts.h
+++ ts.h
@@ -1,34 +1,34 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 typedef struct listidf listidf;
 struct listidf {
-    int state;  
-    char name[20];
-    char code[20];
-    char type[20];
-    float val;
-    char valstr[50];
-    char scope[20];
-    struct listidf* next;
+  int state;
+  char name[20];
+  char code[20];
+  char type[20];
+  float val;
+  char valstr[50];
+  char scope[20];
+  struct listidf* next;
 };
 typedef struct m m;
 
-    struct m {
-        int state;
-        char name[20];
-        char type[20];
-        struct m* next;
-    };
+struct m {
+  int state;
+  char name[20];
+  char type[20];
+  struct m* next;
+};
 
 typedef struct s s;
-    struct s {
-        int state;
-        char name[20];
-        char type[20];
-        struct s* next;
-    };
+struct s {
+  int state;
+  char name[20];
+  char type[20];
+  struct s* next;
+};
 
 listidf* symbolTable;
 m* keywordTable;
@@ -39,238 +39,221 @@ int cpt = 0;
 int cptm = 0;
 int cpts = 0;
 void initialisation() {
-    symbolTable = NULL;
-    keywordTable = NULL;
-    separatorTable = NULL;
-
+  symbolTable = NULL;
+  keywordTable = NULL;
+  separatorTable = NULL;
 }
 
 void inserer(char entite[], char code[], char type[], float val, char valstr[], int y, char scope[]) {
-    if (y == 0) {
-        listidf *newEntry = (listidf*)malloc(sizeof(listidf));
-        newEntry->state = 1;
-        strcpy(newEntry->name, entite);
-        strcpy(newEntry->code, code);
-        strcpy(newEntry->type, type);
-        newEntry->val = val;
-        strcpy(newEntry->scope, scope);
-        strcpy(newEntry->valstr, valstr);
-        newEntry->next = NULL;
+  if (y == 0) {
+    listidf* newEntry = (listidf*)malloc(sizeof(listidf));
+    newEntry->state = 1;
+    strcpy(newEntry->name, entite);
+    strcpy(newEntry->code, code);
+    strcpy(newEntry->type, type);
+    newEntry->val = val;
+    strcpy(newEntry->scope, scope);
+    strcpy(newEntry->valstr, valstr);
+    newEntry->next = NULL;
 
-        if (symbolTable == NULL) {
-            symbolTable = newEntry;
-        } else {
-            listidf* last = symbolTable;
-            while (last->next != NULL) {
-                last = last->next;
-            }
-            last->next = newEntry;
-        }
-        cpt++;
-    } else if (y == 1) {
-        m* newEntry = (m*)malloc(sizeof(m));
-        newEntry->state = 1;
-        strcpy(newEntry->name, entite);
-        strcpy(newEntry->type, code);
-        newEntry->next = NULL;
-
-        if (keywordTable == NULL) {
-            keywordTable = newEntry;
-        } else {
-            m* last = keywordTable;
-            while (last->next != NULL) {
-                last = last->next;
-            }
-            last->next = newEntry;
-        }
-        cptm++;
-    } else if (y == 2) {
-        s* newEntry = (s*)malloc(sizeof(s));
-        newEntry->state = 1;
-        strcpy(newEntry->name, entite);
-        strcpy(newEntry->type, code);
-        newEntry->next = NULL;
-
-        if (separatorTable == NULL) {
-            separatorTable = newEntry;
-        } else {
-            s* last = separatorTable;
-            while (last->next != NULL) {
-                last = last->next;
-            }
-            last->next = newEntry;
-        }
-        cpts++;
+    if (symbolTable == NULL) {
+      symbolTable = newEntry;
+    } else {
+      listidf* last = symbolTable;
+      while (last->next != NULL) {
+        last = last->next;
+      }
+      last->next = newEntry;
     }
+    cpt++;
+  } else if (y == 1) {
+    m* newEntry = (m*)malloc(sizeof(m));
+    newEntry->state = 1;
+    strcpy(newEntry->name, entite);
+    strcpy(newEntry->type, code);
+    newEntry->next = NULL;
 
+    if (keywordTable == NULL) {
+      keywordTable = newEntry;
+    } else {
+      m* last = keywordTable;
+      while (last->next != NULL) {
+        last = last->next;
+      }
+      last->next = newEntry;
+    }
+    cptm++;
+  } else if (y == 2) {
+    s* newEntry = (s*)malloc(sizeof(s));
+    newEntry->state = 1;
+    strcpy(newEntry->name, entite);
+    strcpy(newEntry->type, code);
+    newEntry->next = NULL;
+
+    if (separatorTable == NULL) {
+      separatorTable = newEntry;
+    } else {
+      s* last = separatorTable;
+      while (last->next != NULL) {
+        last = last->next;
+      }
+      last->next = newEntry;
+    }
+    cpts++;
+  }
 }
-
-
 
 void afficher() {
-    listidf* courant = symbolTable;
-    printf("\n\n/****************** Table des symboles ******************/\n\n");
-    printf("\n/*************** Table des symboles IDF ***************/\n");
-    printf("___________________________________________________________________________________________\n");
-    printf("\t|   Nom_Entite   |  Code_Entite   |   Type_Entite   |   Val_Entite    |    scope   |\n");
-    printf("___________________________________________________________________________________________\n");
+  listidf* courant = symbolTable;
+  printf("\n\n/****************** Table des symboles ******************/\n\n");
+  printf("\n/*************** Table des symboles IDF ***************/\n");
+  printf("___________________________________________________________________________________________\n");
+  printf("\t|   Nom_Entite   |  Code_Entite   |   Type_Entite   |   Val_Entite    |    scope   |\n");
+  printf("___________________________________________________________________________________________\n");
 
-    while (courant != NULL) {
-        if ((strcmp(courant->type, "chainec") == 0) || (strcmp(courant->type, "LOGICAL") == 0)) {
-            printf("\t|%15s |%15s | %15s | %15s | %10s |\n", courant->name, courant->code, courant->type, courant->valstr, courant->scope);
-        } else {
-            printf("\t|%15s |%15s | %15s | %15f | %10s |\n", courant->name, courant->code, courant->type, courant->val, courant->scope);
-        }
-        courant = courant->next;
+  while (courant != NULL) {
+    if ((strcmp(courant->type, "chainec") == 0) || (strcmp(courant->type, "LOGICAL") == 0)) {
+      printf("\t|%15s |%15s | %15s | %15s | %10s |\n", courant->name, courant->code, courant->type, courant->valstr, courant->scope);
+    } else {
+      printf("\t|%15s |%15s | %15s | %15f | %10s |\n", courant->name, courant->code, courant->type, courant->val, courant->scope);
     }
+    courant = courant->next;
+  }
 
-    
-    m* current1 = keywordTable;
-printf("\n/*************** Table des symboles mots cles ***************\n");
-printf("___________________________________________\n");
-printf("\t|    NomEntite   |    CodeEntite  | \n");
-printf("___________________________________________\n");
+  m* current1 = keywordTable;
+  printf("\n/*************** Table des symboles mots cles ***************\n");
+  printf("___________________________________________\n");
+  printf("\t|    NomEntite   |    CodeEntite  | \n");
+  printf("___________________________________________\n");
 
+  while (current1 != NULL) {
+    printf("\t|%15s |%15s | \n", current1->name, current1->type);
+    current1 = current1->next;
+  }
 
-    while (current1 != NULL) {
-        printf("\t|%15s |%15s | \n", current1->name, current1->type);
-        current1 = current1->next;
-    }
+  s* current2 = separatorTable;
+  printf("\n/*************** Table des symboles separateurs ***************\n");
+  printf("___________________________________________\n");
+  printf("\t|    NomEntite   |    CodeEntite  | \n");
+  printf("___________________________________________\n");
 
+  while (current2 != NULL) {
+    printf("\t|%15s |%15s | \n", current2->name, current2->type);
+    current2 = current2->next;
+  }
 
-    s* current2 = separatorTable;
-printf("\n/*************** Table des symboles separateurs ***************\n");
-printf("___________________________________________\n");
-printf("\t|    NomEntite   |    CodeEntite  | \n");
-printf("___________________________________________\n");
-
-    while (current2 != NULL) {
-        printf("\t|%15s |%15s | \n", current2->name, current2->type);
-        current2 = current2->next;
-    }
-
-    printf("\n");
-
-
- 
-
+  printf("\n");
 }
 
-
 void rechercher(char entite[], char code[], char type[], float val, char valstr[], int y, char scope[]) {
-    int i;
+  int i;
 
-    switch (y) {
-   case 0: 
-    if (cpt == 0) {
+  switch (y) {
+    case 0:
+      if (cpt == 0) {
         inserer(entite, code, type, val, valstr, 0, scope);
-    } else {
+      } else {
         listidf* current = symbolTable;
         while (current != NULL && current->state == 1) {
-            if (strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
-                // variable existe deja avec le meme scope et le meme nom
-                return;
-            }
-            current = current->next;
+          if (strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
+            // variable existe deja avec le meme scope et le meme nom
+            return;
+          }
+          current = current->next;
         }
 
         // variable n'existe pas
         inserer(entite, code, type, val, valstr, 0, scope);
-    }
-    break;
+      }
+      break;
     case 1:
-            if(cptm == 0) {
-                inserer(entite, code, type, val, valstr, 1, "");
-            }
-            else {
-                m* current = keywordTable;
-                while (current != NULL && strcmp(entite, current->name) != 0 && current->state == 1) {
-                    current = current->next;
-                }
+      if (cptm == 0) {
+        inserer(entite, code, type, val, valstr, 1, "");
+      } else {
+        m* current = keywordTable;
+        while (current != NULL && strcmp(entite, current->name) != 0 && current->state == 1) {
+          current = current->next;
+        }
 
-                if (current == NULL) {
-                    inserer(entite, code, type, val, valstr, 1, "");
-                }
-            }
-            break;
+        if (current == NULL) {
+          inserer(entite, code, type, val, valstr, 1, "");
+        }
+      }
+      break;
 
-        case 2: 
-            if(cpts == 0) {
-                inserer(entite, code, type, val, valstr, 2, "");
-            }
-            else {
-                s* current = separatorTable;
-                while (current != NULL && strcmp(entite, current->name) != 0 && current->state == 1) {
-                    current = current->next;
-                }
+    case 2:
+      if (cpts == 0) {
+        inserer(entite, code, type, val, valstr, 2, "");
+      } else {
+        s* current = separatorTable;
+        while (current != NULL && strcmp(entite, current->name) != 0 && current->state == 1) {
+          current = current->next;
+        }
 
-                if (current == NULL) {
-                    inserer(entite, code, type, val, valstr, 2, "");
-                }
-            }
-            break;
-    }
+        if (current == NULL) {
+          inserer(entite, code, type, val, valstr, 2, "");
+        }
+      }
+      break;
+  }
 }
 
-
 int Recherche_position(char entite[]) {
-    int i = 0;
-    listidf* current = symbolTable;
-    while (current != NULL) {
-        if (strcmp(entite, current->name) == 0) {
-            return i;
-        }
-        current = current->next;
-        i++;
+  int i = 0;
+  listidf* current = symbolTable;
+  while (current != NULL) {
+    if (strcmp(entite, current->name) == 0) {
+      return i;
     }
-    return -1;  
+    current = current->next;
+    i++;
+  }
+  return -1;
 }
 
 void insererTYPE(char entite[], char type[], char scope[]) {
-    listidf* current = symbolTable;
-    while (current != NULL) {
-        if (strcmp(current->code, "IDF") == 0 && strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
-            strcpy(current->type, type);
-        }
-        current = current->next;
+  listidf* current = symbolTable;
+  while (current != NULL) {
+    if (strcmp(current->code, "IDF") == 0 && strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
+      strcpy(current->type, type);
     }
+    current = current->next;
+  }
 }
-
 
 bool doubleDeclaration(char entite[], char scope[]) {
-    listidf* current = symbolTable;
-    while (current != NULL) {
-        if (strcmp(current->code, "IDF") == 0 && strcmp(current->type, "") != 0 && strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
-            return true;  
-        }
-        current = current->next;
+  listidf* current = symbolTable;
+  while (current != NULL) {
+    if (strcmp(current->code, "IDF") == 0 && strcmp(current->type, "") != 0 && strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
+      return true;
     }
-    return false;  
+    current = current->next;
+  }
+  return false;
 }
 
-
-char *type_idf(char entite[], char scope[]) {
-    listidf* current = symbolTable;
-    while (current != NULL) {
-        if (strcmp(current->code, "IDF") == 0 && strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
-            return current->type;
-        }
-        current = current->next;
+char* type_idf(char entite[], char scope[]) {
+  listidf* current = symbolTable;
+  while (current != NULL) {
+    if (strcmp(current->code, "IDF") == 0 && strcmp(entite, current->name) == 0 && strcmp(current->scope, scope) == 0) {
+      return current->type;
     }
-    return "";  
+    current = current->next;
+  }
+  return "";
 }
 
-// la fonction pour recuperer la dimension d'une variable pour tester 
+// la fonction pour recuperer la dimension d'une variable pour tester
 int getDimension(char entite[], char scope[]) {
-    listidf* current = symbolTable;
-    while (current != NULL) {
-        if (strcmp(current->code, "IDF") == 0 &&
-            strcmp(current->name, entite) == 0 &&
-            strcmp(current->scope, scope) == 0) {
-            if (strstr(current->type, "(,)") != NULL) return 2; 
-             if (strstr(current->type, "()") != NULL) return 1; 
-        }
-        current = current->next;
+  listidf* current = symbolTable;
+  while (current != NULL) {
+    if (strcmp(current->code, "IDF") == 0 &&
+        strcmp(current->name, entite) == 0 &&
+        strcmp(current->scope, scope) == 0) {
+      if (strstr(current->type, "(,)") != NULL) return 2;
+      if (strstr(current->type, "()") != NULL) return 1;
     }
-    return 0;  
+    current = current->next;
+  }
+  return 0;
 }
